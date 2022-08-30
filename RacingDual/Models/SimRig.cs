1ca811dc3539bcf7a5db4145e8befb361bbaf9b3
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-08-30 13:26:22 -0600

    finished models for simRig

--- RacingDual/Models/SimRig.cs
+++ RacingDual/Models/SimRig.cs
@@ -1,12 +1,29 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RacingDual.Models
 {
     public class SimRig
     {
-        
+        public int Id { get; set; }
+
+        public string CreatorId { get; set; }
+
+        public Profile Creator { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string Console { get; set; }
+
+        public string Chassis { get; set; }
+
+        public string MonitorStand { get; set; }
+
+        public string Pedal { get; set; }
+
+        public string Software { get; set; }
+
+        public string Extras { get; set; }
     }
 }
