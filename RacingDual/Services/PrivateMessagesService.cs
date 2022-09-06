97290041cfeac8b8939cd3a03c4ce3629ff7b353
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-09-06 16:08:47 -0600

    created table for post comments

--- RacingDual/Services/PrivateMessagesService.cs
+++ RacingDual/Services/PrivateMessagesService.cs
@@ -1,7 +1,12 @@
+using RacingDual.Models;
+
 namespace RacingDual.Services
 {
     public class PrivateMessagesService
     {
+        internal PrivateMessages SendMessage(int profileId, string userId, PrivateMessages messageData)
+        {
 
+        }
     }
 }
