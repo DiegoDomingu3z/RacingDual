daa52a13b72863a94dfefebe9ce5ea8be4d0a4b9
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-09-06 16:12:40 -0600

    private messages in the works

--- RacingDual/Repositories/PrivateMessagesRepository.cs
+++ RacingDual/Repositories/PrivateMessagesRepository.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Data;
+using RacingDual.Models;
+
 namespace RacingDual.Repositories
 {
     public class PrivateMessagesRepository
     {
+        private readonly IDbConnection _db;
 
+        public PrivateMessagesRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        internal PrivateMessages SendMessage(PrivateMessages messageData)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- RacingDual/Services/PrivateMessagesService.cs
+++ RacingDual/Services/PrivateMessagesService.cs
@@ -1,12 +1,20 @@
 using RacingDual.Models;
+using RacingDual.Repositories;
 
 namespace RacingDual.Services
 {
     public class PrivateMessagesService
     {
+        private readonly PrivateMessagesRepository _repo;
+
+        public PrivateMessagesService(PrivateMessagesRepository repo)
+        {
+            _repo = repo;
+        }
+
         internal PrivateMessages SendMessage(int profileId, string userId, PrivateMessages messageData)
         {
-
+            return _repo.SendMessage(messageData);
         }
     }
 }
