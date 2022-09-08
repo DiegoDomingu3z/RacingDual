ec0e6b0aa2d5c99a81298b6555269a5c8515fde2
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-09-08 15:54:38 -0600

    working on getting messages by chat

--- RacingDual/Controllers/AccountController.cs
+++ RacingDual/Controllers/AccountController.cs
@@ -15,9 +15,12 @@ namespace RacingDual.Controllers
     {
         private readonly AccountService _accountService;
 
-        public AccountController(AccountService accountService)
+        private readonly PrivateMessagesService _pms;
+
+        public AccountController(AccountService accountService, PrivateMessagesService pms)
         {
             _accountService = accountService;
+            _pms = pms;
         }
 
         [HttpGet]
@@ -34,7 +37,27 @@ namespace RacingDual.Controllers
                 return BadRequest(e.Message);
             }
         }
-    }
 
+
+        [HttpGet("messages/{id}/profileUser")]
+        [Authorize]
+        public async Task<ActionResult<List<PrivateMessages>>> MessagesWithUser(int id)
+        {
+            try
+            {
+                Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                List<PrivateMessages> messages = _pms.MessageWithUser(id, userInfo.Id);
+                return Ok(messages);
+
+
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+
+            }
+        }
+
+    }
 
 }
