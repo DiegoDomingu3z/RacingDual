1b56cccd85c0572aaccbd85815588ad9ab8a39d9
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-09-19 12:02:24 -0600

    getting all chats in chatroom with all info

--- RacingDual/Services/ChatsService.cs
+++ RacingDual/Services/ChatsService.cs
@@ -39,11 +39,17 @@ namespace RacingDual.Services
         {
             ChatRoom room = _crs.GetChatRoom(chatRoomId, id);
 
-            if (room.AccountId != id || room.ProfileId != id)
+            if (room.AccountId != id && room.ProfileId != id)
             {
-                throw new Exception("This chat room does not exists"); //It does its just not theirs
+                throw new Exception("This chat room was not found"); //It does its just not theirs
             }
-            return _repo.ChatsInChatRoom(chatRoomId);
+            List<Chat> chats = _repo.ChatsInChatRoom(chatRoomId);
+            foreach (var c in chats)
+            {
+                c.UserProfileName = room.ProfileName;
+                c.UserProfilePic = room.ProfilePic;
+            }
+            return chats;
         }
 
         internal Chat GetChatById(int id, string userId)
