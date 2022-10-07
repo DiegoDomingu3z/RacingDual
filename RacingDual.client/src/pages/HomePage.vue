628f05eb65c07f2761ae6bbaf436283363277128
DiegoDomingu3z <diegodominguezf40@gmail.com>
2022-10-06 23:20:33 -0600

    front end work

--- RacingDual.client/src/pages/HomePage.vue
+++ RacingDual.client/src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="img">
-    <HomeTop />
-  </div>
+  <div class="img"></div>
+  <div class="img"></div>
+  <div class="img"></div>
 </template>
 
 
@@ -13,15 +13,27 @@ import HomeTop from '../components/Home/HomeTop.vue'
 import MobileNavbar from '../components/MobileView/MobileNavbar.vue'
 export default {
   setup() {
+    var navbar = document.getElementById("navbar")
+    var sticky = navbar.offsetTop
     onMounted(async () => {
       try {
         await formulaOneService.getCurrentSeasonSchedule();
+        window.onscroll = function () { myFunction() }
       }
       catch (error) {
+
       }
     });
     return {
+      myFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      },
       f1: computed(() => AppState.f1)
+
     };
   },
   components: { HomeTop }
@@ -31,7 +43,7 @@ export default {
 
 <style lang="scss" scoped>
 .img {
-  background-image: url("https://images.unsplash.com/flagged/photo-1557050688-a224243c012f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+  background-image: url("https://images.unsplash.com/photo-1593693044238-f14e5b164e75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80");
   height: 50vh;
   width: 100%;
   background-position: center;
